70b4b7b1e183fae3c906e87dec1e05a91b266199
Jessie Moura <jessie.moura19@gmail.com>
2023-05-25 15:50:12 -0300

    fix: Routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 /* I don't quite know, but I think in here is meant to be the express lib. */
 /* Express is meant to build the data storage routes, right? */
 import express from 'express'
+import Routes from './routes';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use(Routes)
 
 app.listen(port, () => {
     console.log(`Started! Listening port: ${port}`);
